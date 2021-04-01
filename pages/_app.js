1166f0e00e4371273bfa90d28616a5d16fdc1259
Long Trần (KMASoft) <tranlong.kma@gmail.com>
2021-04-01 20:20:44 +0700

    fix: del socket

--- pages/_app.js
+++ pages/_app.js
@@ -11,45 +11,45 @@ import MainContent from "../components/website/contexts/MainContent";
 const SERVER = "http://103.90.226.237:3000";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    ConfigLive.consoleHandle();
-    return () => {};
-  }, []);
+  // useEffect(() => {
+  //   ConfigLive.consoleHandle();
+  //   return () => {};
+  // }, []);
 
-  useEffect(() => {
-    if (typeof io !== "undefined" && io) {
-      let socket = io.connect(SERVER);
+  // useEffect(() => {
+  //   if (typeof io !== "undefined" && io) {
+  //     let socket = io.connect(SERVER);
 
-      socket.on("connect", () => {
-        console.log("socket", socket);
+  //     socket.on("connect", () => {
+  //       console.log("socket", socket);
 
-        socket.emit("events", { views: 1 });
+  //       socket.emit("events", { views: 1 });
 
-        socket.emit("Identity", 0, (response) => {
-          console.log("RES Identity: ", response);
-        });
-        socket.on("events", function (data) {
-          console.log("Events, ", data);
-        });
+  //       socket.emit("Identity", 0, (response) => {
+  //         console.log("RES Identity: ", response);
+  //       });
+  //       socket.on("events", function (data) {
+  //         console.log("Events, ", data);
+  //       });
 
-        socket.on("exception", function (data) {
-          console.log("Events, ", data);
-        });
+  //       socket.on("exception", function (data) {
+  //         console.log("Events, ", data);
+  //       });
 
-        socket.on("disconnect", function () {
-          console.log("disconnect");
-        });
-      });
-    }
-  }, []);
+  //       socket.on("disconnect", function () {
+  //         console.log("disconnect");
+  //       });
+  //     });
+  //   }
+  // }, []);
 
   return (
     <MainContent>
-      {/* <Component {...pageProps} /> */}
-      <div>
+      <Component {...pageProps} />
+      {/* <div>
         Web đang bảo trì.. Chúng tôi sẽ cập nhật trong thời gian sớm nhất. Xin
         cảm ơn
-      </div>
+      </div> */}
     </MainContent>
   );
 }
