4ac64fe2f986b262541082c108a65fa6ca6fb66d
Long Trần (KMASoft) <tranlong.kma@gmail.com>
2021-04-04 14:49:35 +0700

    change name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "maixep-backend",
+      name: "maixep-fontend",
       watch: false,
       interpreter: "/root/.nvm/versions/node/v14.16.0/bin/node",
       env: {
