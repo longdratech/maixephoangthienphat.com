4557f767d5da7591b7b29464298fee18f14baddb
Long Trần (KMASoft) <tranlong.kma@gmail.com>
2021-04-04 15:15:27 +0700

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps: [
     {
       name: "maixep-fontend",
+      script: "/srv/maixephoangthienphat.com/.next/server/pages/_app.js",
       watch: false,
       interpreter: "/root/.nvm/versions/node/v14.16.0/bin/node",
       env: {
