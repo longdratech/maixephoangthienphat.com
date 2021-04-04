1403155286b346ce227c161a522e11705181c7ce
Long Trần (KMASoft) <tranlong.kma@gmail.com>
2021-04-04 17:14:44 +0700

    nothing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,10 @@ module.exports = {
   apps: [
     {
       name: "maixep-fontend",
-      script: "/srv/maixephoangthienphat.com/.next/server/pages/_app.js",
-      watch: false,
-      interpreter: "/root/.nvm/versions/node/v14.16.0/bin/node",
+      script: "yarn",
+      args: "deploy:prod",
+      watch: true,
+      // interpreter: "/root/.nvm/versions/node/v14.16.0/bin/node",
       env: {
         NODE_ENV: "production",
       },
